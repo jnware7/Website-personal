2525d63ba01e502ec5800e33d6706e0941de4bdf
jnware7 <jnware7@gmail.com>
2017-05-03 17:57:17 -0700

    fixed text decoration

--- jquery.js
+++ jquery.js
@@ -18,11 +18,11 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $("#menu-button").click(function(){
-    	$('.pop-menu2').toggle(function () {
-	    $(".pop-menu2").addClass("pop-menu-show2");
+    $("#aboutme").click(function(){
+    	$('.pop-menu').toggle(function () {
+	    $(".pop-menu").addClass("pop-menu-show");
 	}, function () {
-	    $(".pop-menu2").removeClass("pop-menu-show2");
+	    $(".pop-menu").removeClass("pop-menu-show");
 	})
 
 
